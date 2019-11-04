91bb169ddf3bb135da3c3f4c72a064603f4feb6a
Rishi Soni <52169818+rishiso@users.noreply.github.com>
2019-11-03 21:02:04 -0500

    Update Caeser_Cipher.py

--- Caeser_Cipher.py
+++ Caeser_Cipher.py
@@ -5,18 +5,18 @@ key = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 alphabet_cap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-message = input("Enter a message: ")
+def scramble(message):
+    newMessage = ''
+    for char in message:
+        if char.isalpha() == True and char.islower() == True:
+            newPosition = (alphabet.find(char) + key) % 26
+            newMessage += alphabet[newPosition]
+        elif char.isalpha() == True and char.isupper() == True:
+            newPosition = (alphabet_cap.find(char) + key) % 26
+            newMessage += alphabet_cap[newPosition]
+        else:
+            newMessage += char
 
-newMessage = ''
+    print("Your encrypted message is: " + newMessage)
 
-for char in message:
-    if char.isalpha() == True and char.islower() == True:
-        newPosition = (alphabet.find(char) + key) % 26
-        newMessage += alphabet[newPosition]
-    elif char.isalpha() == True and char.isupper() == True:
-        newPosition = (alphabet_cap.find(char) + key) % 26
-        newMessage += alphabet_cap[newPosition]
-    else:
-        newMessage += char
-
-print("Your encrypted message is: " + newMessage)
+scramble("Hello, my name is Rishi.")
